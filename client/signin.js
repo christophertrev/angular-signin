0d468f3842f36a247005998bddbc48ea9691bb35
Christopher Treviño <christophertrev@gmail.com>
2014-11-29 15:28:15 -0800

    Add ui-router and set up views folder and route in signin.js

--- client/signin.js
+++ client/signin.js
@@ -1,5 +1,14 @@
-angular.module('signin',[])
+angular.module('signin', [
+  'ui.router',
+])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/signup');
+    $stateProvider
+      .state('signup',{
+        url: '/signup',
+        templateUrl: 'views/signin.html'
+      })
+  })
+  .controller('signinForm', function ($scope) {
 
-.controller('signinForm', function ($scope){
-
-})
+  })
